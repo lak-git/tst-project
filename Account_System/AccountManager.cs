0eb4a06e1d1b52a747f74727bcdda6ce3110298a
Lakindu Hesara Perera <lakindupereraap@gmail.com>
2024-11-24 11:44:02 +0530

    feat: Initial

---
+++ Account_System/AccountManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tst_project.Account_System;
+
+namespace tst_project
+{
+    internal class AccountManager
+    {
+        private User CurrentLoggedInUser { get; set; }
+    }
+}
---
+++ Account_System/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tst_project.Account_System
+{
+    internal class User : Session
+    {
+    }
+}
