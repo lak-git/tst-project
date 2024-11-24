4dda38084277b952e2498cc1fb6ef640810bc2d3
Lakindu Hesara Perera <lakindupereraap@gmail.com>
2024-11-24 12:12:36 +0530

    feat: Session and User class roughly made

--- Account_System/Session.cs
+++ Account_System/Session.cs
@@ -8,5 +8,19 @@ namespace tst_project.Account_System
 {
     internal class Session
     {
+        public float Score { get; set; }
+        public float TypingSpeed { get; set; }
+
+        public Session()
+        {
+            this.Score = 0;
+            this.TypingSpeed = 0;
+        }
+
+        public void updateStats(float score, float typingSpeed)
+        {
+            this.Score = score;
+            this.TypingSpeed = typingSpeed;
+        }
     }
 }
--- Account_System/User.cs
+++ Account_System/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,15 @@ namespace tst_project.Account_System
 {
     internal class User : Session
     {
+        public string Username { get; private set; }
+        private string Password { get; set; }
+        private int UserID { get; set; }
+
+        public User(string username, string password, int userID) : base()
+        {
+            this.Username = username;
+            this.Password = password;
+            this.UserID = userID;
+        }
     }
 }
