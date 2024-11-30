e5bb595b25cac9c18bed782d7fe2620377e5af44
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2024-11-30 11:05:50 +0530

    fix: Got rid of old commented out code

--- Game_logic/StatsCalculator.cs
+++ Game_logic/StatsCalculator.cs
@@ -8,23 +8,6 @@ namespace tst_project.Game_logic
 {
     internal class StatsCalculator
     {
-        //public float typingSpeed { get; set; }
-        //public float typingAccuracy { get; set; }
-        //public float score { get; set; }
-
-        //public StatsCalculator(float typingSpeed, float typingAccuracy, float score)
-        //{
-        //    this.typingSpeed = typingSpeed;
-        //    this.typingAccuracy = typingAccuracy;
-        //    this.score = score;
-        //}
-        //public StatsCalculator()
-        //{
-        //    typingSpeed = 0.0f;
-        //    typingAccuracy = 0.0f;
-        //    score = 0.0f;
-        //}
-
         public static float CalcTypingSpeed(string userText, float timeInSeconds)
         {
             string[] words = userText.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
@@ -34,6 +17,7 @@ namespace tst_project.Game_logic
 
             return typingSpeed;
         }
+
         public static float CalcTypingAccuracy(string originalText, string userText)
         {
             float correctChars = 0;
@@ -44,14 +28,15 @@ namespace tst_project.Game_logic
                 if (originalText[i] == userText[i])
                     correctChars++;
             }
-
             float typingAccuracy = (correctChars / originalText.Length) * 100;
+
             return typingAccuracy;
         }
 
         public static float CalcScore(float typingSpeed, float typingAccuracy)
         {
-             float score = typingSpeed * typingAccuracy;
+            float score = typingSpeed * typingAccuracy;
+
             return score;
         }
     }
