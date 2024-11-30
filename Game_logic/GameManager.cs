1ad79be900abfdc8bb17440c6667b0cf67685e45
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2024-11-30 09:51:12 +0530

    feat: GameManager.cs initial

---
+++ Game_logic/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tst_project.Account_System;
+
+namespace tst_project.Game_logic
+{
+    internal class GameManager
+    {
+     
+        public AccountManager Account_Manager = new AccountManager();
+        public TextHandler Text_Handler = new TextHandler();
+        public Session CurrentSession = new Session();
+        public string CurrentText;
+
+    }
+}
