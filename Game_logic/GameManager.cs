e67ca7c8d4336c3cdda0e7997ee2635e4b084094
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2024-11-30 10:54:10 +0530

    feat: Added StatsCalculator constructor

--- Game_logic/GameManager.cs
+++ Game_logic/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using tst_project.Account_System;
@@ -9,19 +11,23 @@ namespace tst_project.Game_logic
 {
     internal class GameManager
     {
-
         public AccountManager Account_Manager;
-        public TextHandler Text_Handler;
         public Session CurrentSession;
+        public StatsCalculator Stats_Calculator;
+        public TextHandler Text_Handler;
         public Timer timer;
         public string CurrentText;
+        public string UserInputText;
 
         public GameManager()
         {
-            this.timer = new Timer();
             this.Account_Manager = new AccountManager();
             this.CurrentSession = new Session();
+            this.Stats_Calculator = new StatsCalculator();
             this.Text_Handler = new TextHandler();
+            this.timer = new Timer();
         }
+
+        
     }
 }
